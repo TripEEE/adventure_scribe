7831ec9c3f9f238177e733258279be0bfdbb9fe9
Pitching <adchir95@gmail.com>
2022-11-29 14:42:44 -0500

    creating the ability to add markers on click

--- adventure_scribe-client/src/components/Mapview.js
+++ adventure_scribe-client/src/components/Mapview.js
@@ -1,45 +1,65 @@
 import './Mapview.scss';
-import { MapContainer, ImageOverlay, Marker, Popup } from 'react-leaflet';
+import { MapContainer, ImageOverlay, Marker, Popup, useMapEvent, useMapEvents } from 'react-leaflet';
 import markerIcon from 'leaflet/dist/images/marker-icon.png';
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
+import { useEffect, useState } from 'react';
+
+function LocationMarker() {
+  const [position, setPosition] = useState([])
+  const markerIconConst = L.icon({
+    iconUrl: markerIcon,
+    iconRetinaUrl: markerIcon,
+    iconAnchor: [12, 42],
+    popupAnchor: [0, -40],
+  })
+
+  const map = useMapEvents({
+    click(e) {
+      console.log(e);
+      let markerClick = [e.latlng.lat, e.latlng.lng];
+      setPosition(prev => [...prev, markerClick]);
+    }
+  })
+
+  return position.map ((marker) => {
+    return (
+    <Marker icon={markerIconConst} position={marker}>
+      <Popup>You are here</Popup>
+    </Marker>
+    )
+  })
+}
 
 function Mapview() {
+
   var bounds = [
     [1000, 0],
     [0, 1500],
   ];
 
-  const markerIconConst = L.icon({
-    iconUrl: markerIcon,
-    iconRetinaUrl: markerIcon,
-    iconAnchor: [5, 55],
-    popupAnchor: [10, -44],
-  })
 
   return (
-      <MapContainer
-        crs={L.CRS.Simple}
+    <MapContainer
+      crs={L.CRS.Simple}
+      bounds={bounds}
+      maxZoom={4.9}
+      minZoom={0}
+      onzoomstart={(e) => console.log(e)}
+      attributionControl={false}
+    >
+      <ImageOverlay
+        url={"https://i.redd.it/a57rfqm9nj071.jpg"}
         bounds={bounds}
-        maxZoom={4.9}
-        minZoom={0}
-        onzoomstart={(e) => console.log(e)}
-        attributionControl={false}
-      >
-        <ImageOverlay
-          url={"https://i.redd.it/a57rfqm9nj071.jpg"}
-          bounds={[
-            [1000, 0],
-            [0, 1500],
-          ]}
-        />
-        <Marker icon={markerIconConst} position={[500, 750]}>
-          <Popup>
-            Test popup.
-          </Popup>
-        </Marker>
-      </MapContainer>
+      />
+      <LocationMarker />
+    </MapContainer>
   );
 };
 
+// <Marker icon={markerIconConst} position={[500, 750]}>
+//   <Popup>
+//     Test popup.
+//   </Popup>
+// </Marker>
 export default Mapview;
