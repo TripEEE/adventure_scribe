38802aa94d16f72738e9e9848fc85bb3eaaac82b
Abdulahi hirsi <abdullahi.hirsi@live.com>
2022-11-30 11:11:32 -0500

    added login and regsitration forms

--- adventure_scribe-client/src/components/Application.js
+++ adventure_scribe-client/src/components/Application.js
@@ -1,12 +1,26 @@
 import './Application.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Landing from './Landing'
 import Navbar from './Navbar';
+import Login from './Login'
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 
 function Application() {
   return (
     <main>
+      <Router>
+        <nav>
       <Navbar />
+      </nav>
+          <Routes>
+            <Route path="/landing" element={<Landing/>}/>
+          </Routes>
+          <Routes>
+          <Route path="/login" element={<Login/>}/>
+          </Routes>
+        </Router>
     </main>
+    
   );
 }
 
---
+++ adventure_scribe-client/src/components/Landing.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './Landing.scss';
+
+const Landing = () => {
+  // return (
+  // );
+}
+
+
+
+export default Landing;
