7d3ef32d0c37502f4f66686f2e3abe3c6b7b4807
Pitching <adchir95@gmail.com>
2022-12-03 16:46:52 -0500

    cleaning up edit note view

--- adventure_scribe-client/src/components/Map/NoteViews/Edit.js
+++ adventure_scribe-client/src/components/Map/NoteViews/Edit.js
@@ -3,11 +3,20 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function Edit(props) {
   return (
     <div className="h-100 p-5 text-bg-dark rounded-3">
-      <div className="d-flex justify-content-between">
-        <input type="text" placeholder={props.notes.title}></input>
-        <input type="text" placeholder={props.notes.category}></input>
+      <div className="d-flex flex-row justify-content-between">
+        <div className="d-flex flex-column col-4">
+          <label for="Title">Title</label>
+          <input type="text" name="Title" placeholder={props.notes.title}></input>
+        </div>
+        <div className="d-flex flex-column justify-content-end col-3">
+          <label for="Category">Category</label>
+          <input type="text" name="Category" placeholder={props.notes.category}></input>
+        </div>
       </div>
-      <input type="text" placeholder={props.notes.description}></input>
+      <div className="d-flex flex-column">
+        <label for="Description">Description</label>
+        <textarea name="Description" id="description " placeholder={props.notes.description}></textarea>
+      </div>
       <div className="d-flex justify-content-between">
         <button className="btn btn-outline-light" type="button">Save</button>
         <button className="btn btn-outline-light" type="button" onClick={() => props.setNoteView(null)}>Close</button>
