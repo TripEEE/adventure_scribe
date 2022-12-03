2f9f5e1bd28082a9146cbc7092c8ea18a6409ba4
Pitching <adchir95@gmail.com>
2022-12-03 16:44:03 -0500

    fixing create note view

--- adventure_scribe-client/src/components/Map/NoteViews/Create.js
+++ adventure_scribe-client/src/components/Map/NoteViews/Create.js
@@ -3,11 +3,20 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function Create(props) {
   return (
     <div className="h-100 p-5 text-bg-dark rounded-3">
-      <div className="d-flex justify-content-between">
-        <input type="text"></input>
-        <input type="text"></input>
+      <div className="d-flex flex-row justify-content-between">
+        <div className="d-flex flex-column col-4">
+          <label for="Title">Title</label>
+          <input type="text" name="Title" placeholder="Title"></input>
+        </div>
+        <div className="d-flex flex-column justify-content-end col-3">
+          <label for="Category">Category</label>
+          <input type="text" name="Category" placeholder="Category"></input>
+        </div>
       </div>
-      <input type="text"></input>
+      <div className="d-flex flex-column">
+        <label for="Description">Description</label>
+        <textarea name="Description" id="description "placeholder="Description"></textarea>
+      </div>
       <div className="d-flex justify-content-between">
         <button className="btn btn-outline-light" type="button">Save</button>
         <button className="btn btn-outline-light" type="button" onClick={() => props.setNoteView(null)}>Close</button>
